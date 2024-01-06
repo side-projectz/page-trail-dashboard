864e6c6d0e02e7070c504c92290d8784a3fe8e5f
Surya Umapathy <suryaumapathy2812@gmail.com>
2024-01-06 16:02:16 +0530

    chore: added shadcn/ui components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -74,10 +74,7 @@ const config = {
       },
     },
   },
-  plugins: [
-    require("tailwindcss-animate"),
-    require('@tailwindcss/forms')
-  ],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config
 
 export default config
