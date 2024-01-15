67a64416005b92f5487f29c54a595183a747e2b7
Surya Umapathy <suryaumapathy2812@gmail.com>
2024-01-15 21:51:14 +0530

    fix: Intl default timezone spell fix

--- src/app/(api-routes)/api/extension/route.ts
+++ src/app/(api-routes)/api/extension/route.ts
@@ -4,31 +4,31 @@ import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export const maxDuration = 200;
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
 export async function GET(req: Request) {
   return NextResponse.json({
     status: 200,
-    message: 'API is working',
-    data: null
-  })
+    message: "API is working",
+    data: null,
+  });
 }
-
-
 
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
     const email = body?.email;
     const newRecords = body?.data;
-    const timeZone = body?.timeZone || 'Asia/Calcutta';
+    const timeZone = body?.timeZone || "Asia/Kolkata";
 
     if (!email) throw new Error("Email is required");
     if (typeof email !== "string") throw new Error("Email should be a string");
 
     if (!timeZone) throw new Error("Time Zone is required");
-    if (typeof timeZone !== "string") throw new Error("Time Zone should be a string");
-    if (!Intl.DateTimeFormat().resolvedOptions().timeZone.includes(timeZone)) throw new Error("Invalid Time Zone");
+    if (typeof timeZone !== "string")
+      throw new Error("Time Zone should be a string");
+    if (!Intl.DateTimeFormat().resolvedOptions().timeZone.includes(timeZone))
+      throw new Error("Invalid Time Zone");
 
     if (!newRecords) throw new Error("New Records is required");
 
@@ -36,15 +36,14 @@ export async function POST(req: NextRequest) {
     if (!userDetail) {
       await prisma.user.create({
         data: {
-          email
-        }
-      })
+          email,
+        },
+      });
     }
 
     const pages = newRecords.map((record: any) => record.pages).flat();
 
     for (const site of pages) {
-
       const _startTime = formattedDate(site.openedAt, timeZone);
       const _endTime = formattedDate(site.lastVisited, timeZone);
 
@@ -52,40 +51,40 @@ export async function POST(req: NextRequest) {
         url: site.page,
         meta_title: site.meta.title,
         meta_description: site.meta.description,
-        meta_image: site.meta.image || '',
+        meta_image: site.meta.image || "",
         domain_name: site.domain,
         userId: email,
         startDateTime: _startTime,
         endDateTime: _endTime,
-        timeZone: timeZone
-      })
+        timeZone: timeZone,
+      });
     }
 
-    console.log('User Records Updated')
+    console.log("User Records Updated");
 
     return NextResponse.json({
       message: "User Records Updated",
       data: null,
-      status: 200
+      status: 200,
     });
-
   } catch (error: any) {
     return NextResponse.json({
       message: "Error updating user records, " + error.message,
       data: error,
-      status: 500
+      status: 500,
     });
   }
 }
 
-
-
-const formattedDate = (date: number, timeZone: string) => new Date(new Intl.DateTimeFormat('en-US', {
-  year: "numeric",
-  month: "numeric",
-  day: "numeric",
-  hour: "numeric",
-  minute: "numeric",
-  second: "numeric",
-  timeZone
-}).format(date));
+const formattedDate = (date: number, timeZone: string) =>
+  new Date(
+    new Intl.DateTimeFormat("en-US", {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+      timeZone,
+    }).format(date),
+  );
